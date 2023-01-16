6eff017de534820d3d67849f0b7990516a166076
Eric Landheer <e.landheer@mentechmedia.nl>
2023-01-16 09:57:42 +0100

    :rainbow: Authorize emails

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'ericlandheer95@gmail.com',
+                'info@leader-of-the-pack.nl'
             ]);
         });
     }
